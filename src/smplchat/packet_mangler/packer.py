e315cef5882472c5fe8abc1057ed37e0110c5c98
Viljami Ilola <viljami.ilola@helsinki.fi>
2025-11-18 18:15:46 +0200

    Adds even more packers

--- src/smplchat/packet_mangler/packer.py
+++ src/smplchat/packet_mangler/packer.py
@@ -38,15 +38,46 @@ def packer(m: Message):
             m.sender_local_time,	# L - 4 bytes
             m.old_message_ids,		# 20L - 20x 4 bytes
             m.sender_nick)		# 32s - 32 chars (bytes)
+
     if isinstance(m, KeepaliveRelayMessage):
-        return None
+        return pack(
+            "@BLL",
+            m.msg_type,			# B - 1 byte
+            m.uniq_msg_id,		# L - 4 bytes
+            m.sender_ip)		# L - 4 bytes
+
     if isinstance(m, JoinRequestMessage):
-        return None
+        return pack(
+            "@BLL32s",
+            m.msg_type,			# B - 1 byte
+            m.uniq_msg_id,		# L - 4 bytes
+            m.sender_local_time,	# L - 4 bytes
+            m.sender_nick)		# 32s - 32 chars (bytes)
+
     if isinstance(m, JoinReplyMessage):
-        return None
+        return pack(
+            f"@BL100LLL{len(m.ip_addresses)}",
+            m.msg_type,			# B - 1 byte
+            m.old_message_ids,		# 100L - 100x 4 bytes
+            len(m.ip_addresses),	# L - 4 bytes
+            m.ip_addresses)		# ?L - ? x 4 bytes
+
     if isinstance(m, OldRequestMessage):
-        return None
+        return pack(
+            "@BL",
+            m.msg_type,			# B - 1 byte
+            m.uniq_msg_id)		# L - 4 bytes
+
     if isinstance(m, OldReplyMessage):
-        return None
+        return pack(
+            f"@BBLL32sLs{len(m.old_msg_text)}",
+            m.msg_type,			# B - 1 byte
+            m.old_msg_type,		# B - 1 byte
+            m.old_msg_id,		# L - 4 bytes
+            m.old_sender_local_time,	# L - 4 bytes
+            m.old_sender_nick,		# 32s - 32 chars (bytes)
+            len(m.old_msg_text),	# L - 4 bytes
+            m.old_msg_text)		# ?s - ? * chars
+
     dprint("ERROR: message type not implemented")
     return None
