1306a91487b483731fd66f8f1bc2e33deef20839
Viljami Ilola <viljami.ilola@helsinki.fi>
2025-11-18 20:29:37 +0200

    Fixes types data types and lengths to match the plan

--- src/smplchat/packet_mangler/packer.py
+++ src/smplchat/packet_mangler/packer.py
@@ -19,61 +19,61 @@ def packer(m: Message):
 
     if isinstance(m, ChatRelayMessage):
         return pack(
-            f"@BLLL20L32sLs{len(m.msg_text)}",
+            f"@BQLL32Q32sL{len(m.msg_text)}s",
             m.msg_type,			# B - 1 byte
-            m.uniq_msg_id,		# L - 4 bytes
+            m.uniq_msg_id,		# Q - 8 bytes
             m.sender_ip,		# L - 4 bytes
             m.sender_local_time,	# L - 4 bytes
-            m.old_message_ids,		# 20L - 20x 4 bytes
+            m.old_message_ids,		# 32Q - 32 x 8 bytes
             m.sender_nick,		# 32s - 32 chars (bytes)
             len(m.msg_text),		# L - 4 bytes
             m.msg_text)			# ?s - ? * chars
 
     if isinstance(m, JoinRelayMessage, LeaveRelayMessage):
         return pack(
-            "@BLLL20L32s",
+            "@BQLL32Q32s",
             m.msg_type,			# B - 1 byte
-            m.uniq_msg_id,		# L - 4 bytes
+            m.uniq_msg_id,		# Q - 8 bytes
             m.sender_ip,		# L - 4 bytes
             m.sender_local_time,	# L - 4 bytes
-            m.old_message_ids,		# 20L - 20x 4 bytes
+            m.old_message_ids,		# 32Q - 32x 8 bytes
             m.sender_nick)		# 32s - 32 chars (bytes)
 
     if isinstance(m, KeepaliveRelayMessage):
         return pack(
-            "@BLL",
+            "@BQL",
             m.msg_type,			# B - 1 byte
-            m.uniq_msg_id,		# L - 4 bytes
+            m.uniq_msg_id,		# Q - 8 bytes
             m.sender_ip)		# L - 4 bytes
 
     if isinstance(m, JoinRequestMessage):
         return pack(
-            "@BLL32s",
+            "@BQL32s",
             m.msg_type,			# B - 1 byte
-            m.uniq_msg_id,		# L - 4 bytes
+            m.uniq_msg_id,		# Q - 8 bytes
             m.sender_local_time,	# L - 4 bytes
             m.sender_nick)		# 32s - 32 chars (bytes)
 
     if isinstance(m, JoinReplyMessage):
         return pack(
-            f"@BL100LLL{len(m.ip_addresses)}",
+            f"@B100QL{len(m.ip_addresses)}L",
             m.msg_type,			# B - 1 byte
-            m.old_message_ids,		# 100L - 100x 4 bytes
+            m.old_message_ids,		# 100Q - 100x 8 bytes
             len(m.ip_addresses),	# L - 4 bytes
             m.ip_addresses)		# ?L - ? x 4 bytes
 
     if isinstance(m, OldRequestMessage):
         return pack(
-            "@BL",
+            "@BQ",
             m.msg_type,			# B - 1 byte
-            m.uniq_msg_id)		# L - 4 bytes
+            m.uniq_msg_id)		# Q - 8 bytes
 
     if isinstance(m, OldReplyMessage):
         return pack(
-            f"@BBLL32sLs{len(m.old_msg_text)}",
+            f"@BBQL32sL{len(m.old_msg_text)}s",
             m.msg_type,			# B - 1 byte
             m.old_msg_type,		# B - 1 byte
-            m.old_msg_id,		# L - 4 bytes
+            m.old_msg_id,		# Q - 8 bytes
             m.old_sender_local_time,	# L - 4 bytes
             m.old_sender_nick,		# 32s - 32 chars (bytes)
             len(m.old_msg_text),	# L - 4 bytes
