6497e9cddcf771f753ae67f4ae44700b45cb7b74
Viljami Ilola <viljami.ilola@helsinki.fi>
2025-12-02 12:35:12 +0200

    Adds support to SMPLCHAT_PORT environmental variable

--- src/smplchat/settings.py
+++ src/smplchat/settings.py
@@ -1,9 +1,17 @@
 """ smplchat.settings - Contains global settings like default ports """
 from os import getenv
+from sys import stderr
 
 if "DEBUG" not in globals():
     DEBUG = getenv("DEBUG")
 
-PORT = 62733
+if "PORT" not in globals():
+    PORT = 62733
+    try:
+        PORT = int(getenv("SMPLCHAT_PORT"))
+    except ValueError:
+        print("Ignoring invalid SMPLCHAT_PORT enviromental variable",
+                file=stderr)
+
 NODE_TIMEOUT = 300	# After 300s we can assume connection is lost
-KEEPALIVE_INTERVAL = 30 # keepalive's interval in seconds
+KEEPALIVE_INTERVAL = int(NODE_TIMEOUT/2) # keepalive's interval in seconds
